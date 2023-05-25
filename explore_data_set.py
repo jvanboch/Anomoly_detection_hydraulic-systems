0f3a5f0df5683f86e5028fb9d1211e69206d4a20
Jacob Van Bochove <jvanboch@gmail.com>
2023-05-25 11:50:10 -0700

    updated dash figure to support multiple sensor overlay on plot, and normilization of data

--- explore_data_set.py
+++ explore_data_set.py
@@ -3,10 +3,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import dash
-from dash import  dcc, html, Input, Output
-# import dash_html_components as html
-# import dash_core_components as dcc
-# from plotting_functions import plot_sensor_data_batch_shaded 
+from dash import  dcc, html, Input, Output, State
+from dash.exceptions import PreventUpdate
+import numpy as np
 con = sqlite3.connect("hydraulic_sensor_data.db")
 sensor_df = pd.read_sql_query("SELECT * FROM sensor_batch_data", con)
 sensor_fig=None
@@ -26,35 +25,9 @@ def plot_sensor_data_batch_shaded(sensor_df, sensor_name, batch_num,fig):
 for i in range(0,60):
     sensor_fig = plot_sensor_data_batch_shaded(sensor_df, 'CE', i,sensor_fig)
 
+def NormalizeData(sensor_series):
+    return (sensor_series - np.mean(sensor_series)) / (np.max(sensor_series) - np.min(sensor_series))
 
-# sensor_fig.update_layout(
-#     updatemenus=[
-#         dict(
-#             buttons=list([
-
-#                 dict(
-#                     args=[{'visible':True}],
-#                     label="Batch On",
-#                     method="restyle"
-#                 ),
-#                 dict(
-                   
-#                     args = [{'visible': 'legendonly'}],
-#                     label="Batch Off",
-#                     method="restyle"
-#                 ),
-
-#             ]),
-#             direction="down",
-#             # pad={"r": 10, "t": 10},
-#             showactive=True,
-#             x=0.1,
-#             # xanchor="left",
-#             y=1.2,
-#             yanchor="top"
-#         ),
-#     ]
-# )
 sensensor_fig_html = sensor_fig.to_html()
 with open('html_sensor_data.html','w+') as f:
     f.write(sensensor_fig_html)
@@ -63,7 +36,8 @@ app = dash.Dash()
 app.layout = html.Div(id = 'parent', children = [
     html.H1(id = 'H1', children = 'Sensor Data by Batch', style = {'textAlign':'center',\
                                             'marginTop':40,'marginBottom':40}),
-    dcc.Dropdown( ['CE', 'CP', 'EPS1', 'FS1', 'PS1', 'PS2', 'PS3', 'PS4', 'PS5', 'PS6','SE', 'TS1', 'TS2', 'TS3', 'TS4', 'VS1'], 'CE', id='demo-dropdown'),
+    dcc.Dropdown( ['CE', 'CP', 'EPS1', 'FS1', 'PS1', 'PS2', 'PS3', 'PS4', 'PS5', 'PS6','SE', 'TS1', 'TS2', 'TS3', 'TS4', 'VS1'], ['CE'], id='sensor_drop_down', multi=True),
+    dcc.Dropdown(['Normalized', 'Actual'], 'Actual', id='normalize_button'),
     html.Div(id='dd-output-container'),
         
         dcc.Graph(id = 'line_plot', figure = sensor_fig)    
@@ -71,15 +45,33 @@ app.layout = html.Div(id = 'parent', children = [
                      )
 @app.callback(
     Output('line_plot', 'figure'),
-    Input('demo-dropdown', 'value')
+    Input('sensor_drop_down', 'value'),
+    Input('normalize_button', 'value')
+    # State("sensor_drop_down", "value")
 )
-def update_output(value):
+def update_output(sensor_drop_down, normalize_button):
+    print(sensor_drop_down, normalize_button)
+    value = sensor_drop_down
+    value_norm = normalize_button
+    # print(value, search)_)
+    # if not search_value:
+    #     raise PreventUpdate
     sensor_fig = go.Figure()
-    sensor_fig.add_trace(go.Scatter(y=sensor_df[value],name=value))
-    for i in range(0,60):
-        sensor_fig = plot_sensor_data_batch_shaded(sensor_df, value, i,sensor_fig)
-    return sensor_fig
+    # if sensors==None:
+    #     sensors=['CE']
+    for sensor in value:
+        if value_norm=='Normalized':
+            sensor_series = NormalizeData(sensor_df[sensor])
+        else: 
+            sensor_series = sensor_df[sensor]
+        sensor_fig.add_trace(go.Scatter(y=sensor_series,name=sensor))
 
+    # for i in range(0,60):
+    #     sensor_fig = plot_sensor_data_batch_shaded(sensor_df, sensor, i,sensor_fig)
+
+    print(value)
+   
+    return sensor_fig
 
 if __name__ == '__main__':
      app.run_server(debug=True, use_reloader=False)
